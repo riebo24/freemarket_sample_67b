896e2ed7f6e607087e6333a102c794f038573f5e
YoshinoriMaki <59092156+YoshinoriMaki@users.noreply.github.com>
2020-01-30 17:52:49 +0900

    database mysql

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_072853) do
+ActiveRecord::Schema.define(version: 2020_01_30_084956) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
