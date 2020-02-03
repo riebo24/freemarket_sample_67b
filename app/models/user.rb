3a547bb6c9ed60b9a5a429be4cfaef480cd2d358
YoshinoriMaki <59092156+YoshinoriMaki@users.noreply.github.com>
2020-02-03 15:03:01 +0900

    databaseちょっと変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, presence: true
