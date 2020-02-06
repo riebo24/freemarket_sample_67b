4466dc077012338097154a15d5e220806f225a6c
xxxx <makomakomako19960619@i.softbank.jp>
2020-02-06 17:53:46 +0900

    トップページとデーターベース

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
-  belongs_to :buyer_id
-  belongs_to :seller_id
-  belongs_to :category_id
-  belongs_to :size_id
-  belongs_to :brand_id
+  belongs_to :buyer
+  belongs_to :seller
+  belongs_to :category
+  belongs_to :size
+  belongs_to :brand
+  has_many :images 
 
   validates :deal, inclusion: { in: [true, false] }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'posts#index'
+  root to: 'items#index'
 
 
   resources :comments
