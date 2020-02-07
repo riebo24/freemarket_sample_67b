0e66af6e97aba80a82d3c077f2f5056ee8475c43
rie1224 <a10ea211@yahoo.co.jp>
2020-02-07 11:39:19 +0900

    Cardテーブルを作成
    カラム
    user_id(外部キー)
    customer_id（string）
    card_id（string）
    costomer_idとcard_idは外部キーではない

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,6 @@
 class Card < ApplicationRecord
+  validates :customer_id, presence: true
+  validates :card_id, presence:true
+
+  # customer_id,card_idは、_idがついているものの、外部キーではない
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_084144) do
+ActiveRecord::Schema.define(version: 2020_02_06_040953) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "customer_id"
+    t.string "card_id"
+    t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -65,6 +72,7 @@ ActiveRecord::Schema.define(version: 2020_01_31_084144) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cards", "users"
   add_foreign_key "comments", "items"
   add_foreign_key "comments", "users"
   add_foreign_key "images", "items"
