c8f3346f4bbc3f78e42e69dba0a0c41382a5138c
rie1224 <a10ea211@yahoo.co.jp>
2020-02-07 10:17:28 +0900

    カードテーブルのコントローラを作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,7 @@ class ItemsController < ApplicationController
   def create
     Payjp.api_key = "sk_test_a9eda6b79f76e55fad58497e"
     Payjp::Charge.create(
-      amount: 102, # 決済する値段
+      amount: 105, # 決済する値段
       card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
       currency: 'jpy'
     )
