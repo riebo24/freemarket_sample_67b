836fca82b69e7c202c1527837538b5308864ea7a
rie1224 <a10ea211@yahoo.co.jp>
2020-02-09 00:20:54 +0900

    comit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_kane, :last_kane, :post_number, :prefecture, :city, :house_number, :building_name, :phone_number, :birthday, :card, :profile])
     # devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :passwaord])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+    # devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
   end
 
   def production?
