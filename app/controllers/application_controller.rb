a12439f12efd68fbf33462bb567658bc05ea3c15
rie1224 <a10ea211@yahoo.co.jp>
2020-02-09 12:12:46 +0900

    comit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :authenticate_user! 
+  before_action :authenticate_user! 
   protect_from_forgery with: :exception
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-  end     
+ 
 
   # def configure_permitted_parameters
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_kane, :last_kane, :post_number, :prefecture, :city, :house_number, :building_name, :phone_number, :birthday, :card, :profile])
@@ -17,7 +15,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+ 
 
   def production?
     Rails.env.production?
