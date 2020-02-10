d1a8b24463f9c8d94ae49093f0ddc0c195cdda04
rie1224 <a10ea211@yahoo.co.jp>
2020-02-10 19:45:25 +0900

    comit

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,8 @@ class Item < ApplicationRecord
   has_many   :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 
-  #  validates :deal, default: false,inclusion: { in: [true, false] }
-  #dealのデフォルト値はnull。本番環境でエラーになるため、一旦コメントアウト（吉田）
+   validates :deal, default: false,inclusion: { in: [true, false] }
+
 
 end
 
