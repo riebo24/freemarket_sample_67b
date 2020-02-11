5bd30b04bd4485d18979ff55dfb885241b763ca3
hashimotoakihiko <jap21.wave@gmail.com>
2020-02-11 19:09:26 +0900

    マイグレーションファイル差し替え

--- db/migrate/20200205065720_addreferenc.rb
+++
@@ -1,7 +0,0 @@
-class Addreferenc < ActiveRecord::Migration[5.2]
-  def change
-    add_column :items, :send_address, :string, null: false
-    add_column :items, :send_date, :string, null: false
-    add_column :items, :condition, :string, null: false
-  end
-end
