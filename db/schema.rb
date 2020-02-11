6cd8c16fa34131a7f2f76f95f6b519b58ae9eb55
xxxx <makomakomako19960619@i.softbank.jp>
2020-02-11 12:39:31 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_02_10_065032) do
-
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -52,7 +50,7 @@ ActiveRecord::Schema.define(version: 2020_02_10_065032) do
     t.string "cost", null: false
     t.string "delivery", null: false
     t.bigint "brand_id"
-    t.boolean "deal", default: false, null: false
+    t.boolean "deal"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "seller_id"
